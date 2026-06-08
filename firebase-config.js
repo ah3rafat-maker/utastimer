7bfe157ca0cc413ecf9bc2c8da29e05b9f7eef0c
ah3rafat-maker <ah.3rafat@gmail.com>
2026-06-08 16:39:03 +0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+/* إعدادات Firebase الخاصة بمشروع utastimer */
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBFZbYD4b3h0lbjByzfT3MdG25ad4Phdlk",
+  authDomain: "examtimer-4feef.firebaseapp.com",
+  projectId: "examtimer-4feef",
+  storageBucket: "examtimer-4feef.firebasestorage.app",
+  messagingSenderId: "939875650134",
+  appId: "1:939875650134:web:6fccb6970f418a884d49f7"
+};
